ff588c877eafb7146dcc3af966bdac07f1a3ada5
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-03 20:20:49 +0200

    Updates

---
+++ precache-manifest.3f17f25d9ed312c643329f09f24a963c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020234972605250057ba6fa38531e6fa",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "1999088a8f1bc9020ebb",
+    "url": "/react_static-list-of-todos/static/css/main.ab115ec7.chunk.css"
+  },
+  {
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/react_static-list-of-todos/static/js/2.a7975bab.chunk.js"
+  },
+  {
+    "revision": "1999088a8f1bc9020ebb",
+    "url": "/react_static-list-of-todos/static/js/main.a71af138.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_static-list-of-todos/precache-manifest.1460eb5f6aa52629add7e99c8f3a4e44.js"
+  "/react_static-list-of-todos/precache-manifest.3f17f25d9ed312c643329f09f24a963c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_static-list-of-todos/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
